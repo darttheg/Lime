28a9c193c801c2c43ad93af38810ba45aa727ccf
Mechlus <82886093+darttheg@users.noreply.github.com>
2025-08-10 21:55:12 -0400

    Add class files for Compatibles

--- include/Compatible2D.h
+++ include/Compatible2D.h
@@ -15,87 +15,14 @@ public:
     sol::function onHover;
 
     virtual irr::gui::IGUIElement* getNode() const = 0;
-
-    void setParent(sol::optional<Compatible2D*> parent) {
-        irr::gui::IGUIElement* node = getNode();
-        if (!node || !(*parent)) return;
-
-        // add child etc.
-        (*parent)->getNode()->addChild(node);
-    }
-
-	bool getClickable() {
-		return getNode() ? button != nullptr : false;
-	}
-
-	bool getHovered() {
-		if (!getNode())
-			return false;
-
-		irr::core::position2di mousePos = device->getCursorControl()->getPosition();
-		irr::core::recti rect(
-			getNode()->getAbsoluteClippingRect().UpperLeftCorner,
-			getNode()->getAbsoluteClippingRect().LowerRightCorner
-		);
-		return rect.isPointInside(mousePos);
-	}
-
-	void setHovered() {
-
-	}
-
-	void setClickable(sol::function f) {
-		if (!getNode())
-			return;
-		if (f && !button) {
-			clickable = true;
-			updateButton();
-			receiver->buttonCallbackClick.push_back(ButtonCallbackPairClick(button, f));
-		}
-		else if (!f && button) {
-			receiver->removeImg(button);
-			button->remove();
-			clickable = false;
-		}
-	}
-
-	void setHover(sol::function hov) {
-		if (!getNode()) return;
-
-		if (!clickable) {
-			bool cl = clickable;
-			clickable = true;
-			updateButton();
-			clickable = cl;
-		}
-
-		receiver->buttonCallbackHover.push_back(ButtonCallbackPairHover(button, hov));
-	}
-
-	bool getPressed() {
-		if (!getNode()) return false;
-		return button ? button->isPressed() : false;
-	}
-
-	void updateButton() {
-		if (!clickable)
-			return;
-
-		irr::core::recti r = getNode()->getRelativePosition();
-		if (button)
-			receiver->removeImg(button);
-		button = guienv->addButton(irr::core::recti(0, 0, r.LowerRightCorner.X - r.UpperLeftCorner.X, r.LowerRightCorner.Y - r.UpperLeftCorner.Y), getNode());
-		button->setDrawBorder(false);
-		button->setUseAlphaChannel(true);
-	}
+    void setParent(sol::optional<Compatible2D*> parent);
+    bool getClickable();
+    bool getHovered();
+    void setHovered();
+    void setClickable(sol::function f);
+    void setHover(sol::function hov);
+    bool getPressed();
+    void updateButton();
 };
 
-inline void bindCompatible2D() {
-    sol::usertype<Compatible2D> bind_type = lua->new_usertype<Compatible2D>("Compatible2D",
-		"hovered", sol::property(&Compatible2D::getHovered, &Compatible2D::setHovered),
-		"pressed", sol::property(&Compatible2D::getPressed, &Compatible2D::setHovered));
-
-    bind_type["setParent"] = &Compatible2D::setParent;
-	bind_type["fireOnClick"] = &Compatible2D::setClickable;
-	bind_type["fireOnHover"] = &Compatible2D::setHover;
-}
+void bindCompatible2D();
--- include/Compatible3D.h
+++ include/Compatible3D.h
@@ -9,79 +9,18 @@ class Compatible3D {
 public:
     virtual ~Compatible3D() = default;
 
+    bool hasEntry = false;
+
     virtual irr::scene::ISceneNode* getNode() const = 0;
-
-    void setParent(sol::optional<Compatible3D*> parent) {
-        irr::scene::ISceneNode* node = getNode();
-        if (!node) return;
-
-        node->setParent(*parent ? (*parent)->getNode() : nullptr);
-    }
-
-    Vector3D getAbsPos() {
-        irr::scene::ISceneNode* node = getNode();
-        if (!node) return Vector3D();
-
-        irr::core::vector3df pos = node->getAbsolutePosition();
-        return Vector3D(pos.X, pos.Y, pos.Z);
-    }
-
-    Vector3D getAbsRot() {
-        irr::scene::ISceneNode* node = getNode();
-        if (!node) return Vector3D();
-
-        irr::core::vector3df rot = node->getAbsoluteTransformation().getRotationDegrees();
-        return Vector3D(rot.X, rot.Y, rot.Z);
-    }
-
-    Vector3D getAbsScale() {
-        irr::scene::ISceneNode* node = getNode();
-        if (!node) return Vector3D();
-
-        irr::core::vector3df scale = node->getAbsoluteTransformation().getScale();
-        return Vector3D(scale.X, scale.Y, scale.Z);
-    }
-
-    void updateAbsPos() {
-        if (!getNode()) return;
-
-        getNode()->updateAbsolutePosition();
-    }
-
-    sol::object getEntry() {
-        auto it = irrHandler->comp3dmap.find(getNode());
-        if (it != irrHandler->comp3dmap.end())
-            return it->second;
-        return sol::nil;
-    }
-
-	void setEntry(sol::object obj) {
-		if (obj.is<sol::nil_t>())
-			irrHandler->comp3dmap.erase(getNode());
-		else
-			irrHandler->comp3dmap[getNode()] = obj;
-	}
-
-    void destroyEntry() {
-        auto it = irrHandler->comp3dmap.find(getNode());
-        if (it != irrHandler->comp3dmap.end())
-            irrHandler->comp3dmap.erase(it);
-    }
-
-    void createEntry() {
-        if (irrHandler->comp3dmap.find(getNode()) == irrHandler->comp3dmap.end())
-            irrHandler->comp3dmap[getNode()] = lua->create_table_with();
-    }
+    void setParent(sol::optional<Compatible3D*> parent);
+    Vector3D getAbsPos();
+    Vector3D getAbsRot();
+    Vector3D getAbsScale();
+    void updateAbsPos();
+    sol::table getEntry();
+    void setEntry(sol::table obj);
+    void createEntry();
+    void destroyEntry();
 };
 
-inline void bindCompatible3D() {
-    sol::usertype<Compatible3D> bind_type = lua->new_usertype<Compatible3D>("Compatible3D",
-        "attributes", sol::property(&Compatible3D::getEntry, &Compatible3D::setEntry)
-    );
-
-    bind_type["setParent"] = &Compatible3D::setParent;
-    bind_type["getAbsolutePosition"] = &Compatible3D::getAbsPos;
-    bind_type["getAbsoluteRotation"] = &Compatible3D::getAbsRot;
-    bind_type["getAbsoluteScale"] = &Compatible3D::getAbsScale;
-    bind_type["updateAbsolutePosition"] = &Compatible3D::updateAbsPos;
-}
+void bindCompatible3D();
