a1394c80d1f6ed56428b7a2e60efcb261fc37a1d
Mechlus <mechlusdev@gmail.com>
2025-08-25 20:23:36 -0400

    Rework Compatible2D interaction

--- src/Compatible2D.cpp
+++ src/Compatible2D.cpp
@@ -9,12 +9,33 @@ void Compatible2D::setParent(sol::optional<Compatible2D*> parent) {
 }
 
 void Compatible2D::setDoInputEvents(bool enable) {
-	if (getNode())
-		doEvents = enable;
+	if (!getNode()) return;
+
+	bool old = doEvents;
+	doEvents = enable;
+
+	if (doEvents != old) {
+		if (enable)
+			initButton();
+		else {
+			// TODO: Remove button from receiver callback
+
+			button->drop();
+		}
+	}
 }
 
-void Compatible2D::initButton() {
+void Compatible2D::initButton() { // Called on making clickable or resizing/new texture loaded
+	if (!getNode()) return;
 
+	if (button) button->drop();
+
+	irr::core::recti rect = getNode()->getRelativePosition();
+	rect = irr::core::recti(0, 0, rect.LowerRightCorner.X - rect.UpperLeftCorner.X, rect.LowerRightCorner.Y - rect.UpperLeftCorner.Y);
+
+	button = guienv->addButton(rect, getNode());
+	// button->setVisible(false); Commented for debug purposes
+	// TODO: Add to GUI callbacks
 }
 
 void bindCompatible2D() {
@@ -24,5 +45,5 @@ void bindCompatible2D() {
 	);
 
 	bindType["setParent"] = &Compatible2D::setParent;
-	bindType["setDoInputEvents"] = &Compatible2D::setDoInputEvents;
+	bindType["setInteractable"] = &Compatible2D::setDoInputEvents;
 }
